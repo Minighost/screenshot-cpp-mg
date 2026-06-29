1cefc70c6b91a05feff8bb3135c7bf9266dac98c
Minighost <maesternniw@gmail.com>
2026-06-29 00:44:19 -0700

    formatting

--- generate_qrc_list.py
+++ generate_qrc_list.py
@@ -3,10 +3,10 @@ from pathlib import Path
 CUR_DIR = Path(__file__).parent
 TARGET_DIR = "svgs"
 
-for svg in (CUR_DIR/TARGET_DIR).iterdir():
+for svg in (CUR_DIR / TARGET_DIR).iterdir():
     print(f"<file>{TARGET_DIR}/{svg.name}</file>")
 
 print
 
-for svg in (CUR_DIR/TARGET_DIR).iterdir():
+for svg in (CUR_DIR / TARGET_DIR).iterdir():
     print(f"{TARGET_DIR}/{svg.name}")
